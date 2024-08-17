0c24ce5d79ede18ef3aeca66eed641559a5a477f
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-17 18:08:19 +0530

    AtCoder_beginner_contest_367.cpp

---
+++ AtCoder_beginner_contest_367.cpp
@@ -0,0 +1,31 @@
+//A. Shout Everyday
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b<c){
+        if(a>=b && a<c){
+            cout<<"No\n";
+        }else{
+            cout<<"Yes\n";
+        }
+    }else{
+        if(a>=b || a<c) {
+            cout<<"No\n";
+        }else{
+            cout<<"Yes\n";
+        }
+    }
+    return 0;
+}
+
+//B. Cut.0
+#include<iostream>
+using namespace std;
+int main(){
+    double n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
